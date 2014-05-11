192706a12b5440e61328a79f017300675664d67a
Monster <Monster@monster>
2014-05-11 19:44:47 +0200

    Error in Utilities->join corrected. Finished with method

--- mreilaender_cipher/src/src/reilaender/cipher/Utilities.java
+++ mreilaender_cipher/src/src/reilaender/cipher/Utilities.java
@@ -6,7 +6,16 @@ import java.util.List;
 public class Utilities {
 
 	public static String join(CharSequence[] arr, String delimit) {
-		return "join() in Utilities";
+		if (arr.length == 0) return null;
+        StringBuilder sb = new StringBuilder();
+        System.out.println("new StringBuilder successfull.\n");
+        int i;
+        for(i = 0; i < arr.length - 1; i++) {
+        	
+        	sb.append(arr[i].toString());
+        	sb.append(delimit);
+        }
+        return sb.toString() + arr[i];
 	}
 	
 	public static String repeat(CharSequence cseq, int count) {
